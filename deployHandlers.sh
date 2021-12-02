1733fa58e004d2d5e80897fec74fc961316125db
Hens Laurens (s111021) <laurens.hens@student.ap.be>
2021-12-02 16:25:08 +0100

    Adding deploy code for extra hooks

--- deployHandlers.sh
+++ deployHandlers.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
-export STACK_NAME=<your team name>-htf2021-clueprocessing-handlers
+export STACK_NAME=deparadepaardjes-htf2021-clueprocessing-handlers
 export MY_REGION=eu-central-1
 export MY_DEV_BUCKET=htf-deploymentbucket
 
 # Package new cloudformation package
-aws cloudformation package --template templates/handlers/template.yaml --s3-bucket $MY_DEV_BUCKET --output-template export-notification-channel-handlers.yaml --region $MY_REGION
+aws cloudformation package --template templates/handlers/template.yaml --s3-bucket htf-deploymentbucket --output-template export-notification-channel-handlers.yaml --region eu-central-1
 # Deploy 
-sam deploy --region $MY_REGION --template-file export-notification-channel-handlers.yaml --stack-name $STACK_NAME --capabilities CAPABILITY_NAMED_IAM --parameter-overrides Stage=dev
+sam deploy --region eu-central-1 --template-file export-notification-channel-handlers.yaml --stack-name deparadepaardjes-htf2021-clueprocessing-handlers --capabilities CAPABILITY_NAMED_IAM --parameter-overrides Stage=dev
+
+# Package new cloudformation package
+aws cloudformation package --template templates/handlers/template2.yaml --s3-bucket htf-deploymentbucket --output-template export-notification-channel-handlers2.yaml --region eu-central-1
+# Deploy 
+sam deploy --region eu-central-1 --template-file export-notification-channel-handlers2.yaml --stack-name deparadepaardjes-htf2021-clueprocessing-handlers --capabilities CAPABILITY_NAMED_IAM --parameter-overrides Stage=dev
+
+# Package new cloudformation package
+aws cloudformation package --template templates/handlers/template3.yaml --s3-bucket htf-deploymentbucket --output-template export-notification-channel-handlers3.yaml --region eu-central-1
+# Deploy 
+sam deploy --region eu-central-1 --template-file export-notification-channel-handlers3.yaml --stack-name deparadepaardjes-htf2021-clueprocessing-handlers --capabilities CAPABILITY_NAMED_IAM --parameter-overrides Stage=dev
